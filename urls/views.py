893992e20376019512e6bca88a2fef5f1eff38fd
Cechi Shi <shicechi@gmail.com>
2015-08-22 01:39:58 -0700

    change to function based

--- urls/views.py
+++ urls/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Url
+from rest_framework.decorators import api_view
+from rest_framework import status
+from rest_framework.response import Response
 from .serializers import UrlSerializer
 from .forms import UrlForm
 from django.contrib.auth.decorators import login_required
@@ -35,18 +38,46 @@ def url_add(request):
 
 
 @login_required(login_url='/urlexpander/accounts/login/')
-class UrlList(generics.ListCreateAPIView):
-    queryset = Url.objects.all()
-    serializer_class = UrlSerializer
+@api_view(['GET', 'POST'])
+def snippet_list(request):
+    if request.method == 'GET':
+        urls = Url.objects.all()
+        serializer = UrlSerializer(urls, many=True)
+        return Url(serializer.data)
+    elif request.method == 'POST':
+        serializer = UrlSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 @login_required(login_url='/urlexpander/accounts/login/')
-class UrlDetail(generics.RetrieveDestroyAPIView):
-    queryset = Url.objects.all()
-    serializer_class = UrlSerializer
+@api_view(['GET', 'PUT', 'DELETE'])
+def snippet_detail(request, pk):
+    try:
+        url = Url.objects.get(pk=pk)
+    except Url.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        serializer = UrlSerializer(url)
+        return Response(serializer.data)
+
+    elif request.method == 'PUT':
+        serializer = UrlSerializer(url, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == 'DELETE':
+        url.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 
 @login_required(login_url='/urlexpander/accounts/login/')
+@api_view(['POST', ])
 def recapture(request, pk):
     url = get_object_or_404(Url, pk=pk)
     if request.method == 'POST':
