0747183efb568a1eba0393fe8f8bc8e05cfd06ff
Cechi Shi <shicechi@gmail.com>
2015-08-22 01:17:06 -0700

    added field attr

--- urls/serializers.py
+++ urls/serializers.py
@@ -3,6 +3,13 @@ from .models import Url
 
 
 class UrlSerializer(serializers.ModelSerializer):
+    origin = serializers.URLField(required=True)
+    destination = serializers.URLField(required=False)
+    status = serializers.IntegerField(required=False)
+    title = serializers.CharField(max_length=200, required=False, allow_blank=True)
+    screenshot = serializers.URLField(required=False)
+
     class Meta:
         model = Url
         fields = ('origin', 'destination', 'status', 'title', 'screenshot')
+        read_only_fields = ('destination', 'status', 'title', 'screenshot')
