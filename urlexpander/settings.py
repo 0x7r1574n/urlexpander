a82a052d226b19bbfaf51ce7023706415a853bc2
Cechi Shi <shicechi@gmail.com>
2015-08-21 19:24:53 -0700

    remove static root

--- urlexpander/settings.py
+++ urlexpander/settings.py
@@ -107,7 +107,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 AWS_ACCESS_KEY_ID = AWS['ID']
